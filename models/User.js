673a257c6e2e412918acc0ea5af4aea7ce16892a
eddie Hinojosa <eddie_hinojosa@live.com>
2024-08-06 15:16:30 -0400

    completed the User schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
 
-
+//User Schema
 const UserSchema = new Schema(
     {
         username: {
@@ -17,11 +17,11 @@ const UserSchema = new Schema(
             match: [/.+@.+\..+/, 'Please enter a valid email address']
         },
         thoughts: [{
-            type: Schema.Types.ObjectId,
+            type: mogoose.Schema.Types.ObjectId,
             ref: 'thoughts'
         }],
         friends: [{
-            type: Schema.Types.ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
         }],
     },
@@ -32,3 +32,17 @@ const UserSchema = new Schema(
         id: false
     }
 );
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+// get total count of thoughts on retrieval
+UserSchema.virtual('thoughtCount').get(function() {
+    return this.thoughts.length;
+});
+
+// create the User model using the UserSchema
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
