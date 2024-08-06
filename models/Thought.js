51c71da134315f158608d21b56d02ea52039da4d
eddie Hinojosa <eddie_hinojosa@live.com>
2024-08-06 15:29:52 -0400

    added schema for thoghts and reactions in their respective js files

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,41 @@
 const mongoose = require('mongoose');
 const User = require('./User.js');
 const Reaction = require('./Reaction.js');
+const ReactionSchema = require('./Reaction.js').schema;
+
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema] //pulling from Reaction.js
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+
+const Thought = mongoose.model('Thoughts', thoughtSchema);
+
+export default Thought;
