1bb52ee5526e0414fa12d15e2815c1a8972c0671
eddie Hinojosa <eddie_hinojosa@live.com>
2024-08-07 10:37:57 -0400

    added first thought CRUD post

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,44 @@
 const router = require('express').Router();
+const express = require('express');
 const User = require('../../models/User');
 const Thought = require('../../models/Thought');
 
 
+//------------thoughts Routes----------------
+// CRUD Create/post
+
+router.post('/:id', async (req, res)=> {
+    try {
+        const thought = await Thought.create(req.body);
+        await User.findOneAndUpdate(
+            { _id: req.params.id },
+            { $push: { thoughts: thought._id } },
+            { new: true }
+        );
+        res.json({message: 'Thought added to user', data: thought});
+    } catch (err) {
+        res.status(400).json({message: 'Unable to add thought', error: err});
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
